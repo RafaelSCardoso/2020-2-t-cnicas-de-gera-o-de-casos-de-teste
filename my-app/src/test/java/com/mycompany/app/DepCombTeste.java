931181cdaa7b253e99523c2cadb3cc9d102eab01
RafaelSCardoso <rafaelcardoso2604@gmail.coim>
2020-10-07 13:26:40 -0300

    testes parametrizados +config pom

--- my-app/src/test/java/com/mycompany/app/DepCombTeste.java
+++ my-app/src/test/java/com/mycompany/app/DepCombTeste.java
@@ -1,12 +1,20 @@
 package com.mycompany.app;
 
+import java.util.Collection;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class DepCombTeste {
     private DepComb tanques;
-    private enum TIPOPOSTO { COMUM, ESTRATEGICO }
 
     @BeforeAll
     public void setup() {
@@ -49,237 +57,63 @@ public class DepCombTeste {
         new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
         
     }
-
-
-
-
-    // ----------------------  Tests encomendaCombustivel ----------------------
-
-    @Test
-    public void recebeInicialTeste() {
-        int[] quantTanque={0,0,0,0};
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        Assertions.assertEquals(500, tanques.recebeAditivo(500));
-        Assertions.assertEquals(10000, tanques.recebeGasolina(10000));
-        Assertions.assertEquals(2500, tanques.recebeAlcool(2500));
-    }
-
-    @Test
-    public void recebeMetadeTeste() {
-        int[] quantTanque={250,5000,625,625};
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        Assertions.assertEquals(250, tanques.recebeAditivo(500));
-        Assertions.assertEquals(5000, tanques.recebeGasolina(6000));
-        Assertions.assertEquals(1250, tanques.recebeAlcool(2000));
-    }
-
-    @Test
-    public void recebeOverflowTeste() {
-        int[] quantTanque={500,10000,1250,1250};
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        Assertions.assertEquals(0, tanques.recebeAditivo(1));
-        Assertions.assertEquals(0, tanques.recebeGasolina(1));
-        Assertions.assertEquals(0, tanques.recebeAlcool(1));
-    }
-
-    @Test
-    public void recebeInvalidoTeste() {
-        int[] quantTanque={0,0,0,0};
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        Assertions.assertEquals(-1, tanques.recebeAditivo(-50));
-        Assertions.assertEquals(-1, tanques.recebeGasolina(-50));
-        Assertions.assertEquals(-1, tanques.recebeAlcool(-50));
-    }
-
     
-
-
-
-
-    // ----------------------  Tests encomendaCombustivel ----------------------
-    // ----------------------  >=50%  ----------------------
+       
     
-
-    @Test
-    public void Test1() {
-        int[] quantTanque={
-            500,
-            10000,
-            1250,
-            1250
-        };
-        int pedido = 500;
-
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        int [] result =
-            {
-                475,
-                9650,
-                1125,
-                1250
-
-            };
-        // testa posto comum
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.COMUM));
-
-        // testa posto estrategico
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.ESTRATEGICO));
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-    }
-
-    // implementar secondTest? Excel
-
-    @Test
-    // sobrecarga no pedido
-    public void Test2() {
-        int[] quantTanque={
-            500,
-            10000,
-            1250,
-            1250
-        };
-        int pedido = 10500;
-
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        // testa posto comum
-        int [] resultComum = {-3,0,0,0};
-        Assertions.assertEquals(resultComum, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.COMUM));
+    // ----------------------  Tests Construtor ----------------------
+    @ParameterizedTest
+    @CsvSource({
+        "0,0,0,0,500,500,10000,10000,2500,2500",
+        "250,5000,625,625,250,500,5000,6000,1250,2000",
+        "500,10000,1250,1250,0,1,0,1,0,1",
+        "500,10000,1250,1250,-1,-50,-1,-50,-1,-50",
+    })
+    public void testeConstrutor(
+        int quantTAditivo,int  quantTGasolina,int  quantTAlcool1,int  quantTAlcool2,
+        int respAditivo, int enviaAditivo,
+        int respGasolina, int enviaGasolina,
+        int respAlcool, int enviaAlcool
+        ){
+            tanques = new DepComb(quantTAditivo, quantTGasolina, quantTAlcool1, quantTAlcool2);
+            Assertions.assertEquals(respAditivo, tanques.recebeAditivo(enviaAditivo));
+            Assertions.assertEquals(respGasolina, tanques.recebeGasolina(enviaGasolina));
+            Assertions.assertEquals(respAlcool, tanques.recebeAlcool(enviaAlcool));
         
-        int [] result =
-            {
-            0,
-            7000,
-            0,
-            0
-
-            };
-        // testa posto estrategico
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.ESTRATEGICO));
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
     }
+     
+    // ----------------------  Tests encomendaCombustivel ----------------------
+    
+    @ParameterizedTest
+    @CsvSource({
+        //Quant. I. Tanques         Quant. F. Comum         Quant. F. Estrategica   Pedido
+        // ----------------------  >=50%  ----------------------
+        "500,10000,1250,1250,       475,9650,1125,1250,     475,9650,1125,1250,     500",
+        "500,10000,1250,1250,       -3,0,0,0,               0,7000,0,0,             10500",
+        
+        // ----------------------  <50%  ----------------------
+        "499,4999,1250,1250,        237,4987,1238,1250,     224,4974,1225,1250,     500",
+        "125,2500,313,313,          113,2488,300,313,       100,2475,288,313,       500",
+
+        // ----------------------  <25%  ----------------------
+        "124,2500,313,313,          -2,0,0,0,               99,2475,288,313,        500",
+        "20,90,100,30,              -1,0,0,0,               -1,0,0,0,               -50",
 
 
-
-    // ----------------------  <50%  ----------------------
-
-    @Test
-    public void Test3() {
-        int[] quantTanque={
-            249,
-            4999,
-            1250,
-            1250
-        };
-        int pedido = 500;
-
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        int [] result =
-            {
-                237,
-                4987,
-                1238,
-                1250
-
-            };
-        // testa posto comum
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.COMUM));
-
-        // testa posto estrategico
-        int [] result2 =
-            {
-                224,
-                4974,
-                1225,
-                1250 
-            };
+    })
+    public void testeEncomenda(
+        int quantTAditivo,int  quantTGasolina,int  quantTAlcool1,int  quantTAlcool2,
+        int quantTAditivoResultComum,int  quantTGasolinaResultComum,int  quantTAlcool1ResultComum,int  quantTAlcool2ResultComum,
+        int quantTAditivoResultEstrategico,int  quantTGasolinaResultEstrategico,int  quantTAlcool1ResultEstrategico,int  quantTAlcool2ResultEstrategico,
+        int pedido
+        ){
+            tanques = new DepComb(quantTAditivo, quantTGasolina, quantTAlcool1, quantTAlcool2);
+            int[] resultComum = {quantTAditivoResultComum, quantTGasolinaResultComum, quantTAlcool1ResultComum, quantTAlcool2ResultComum}; 
+            Assertions.assertEquals(resultComum, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.COMUM));
             
-        Assertions.assertEquals(result2, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.ESTRATEGICO));
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-    }
-
-    @Test
-    public void Test4() {
-        int[] quantTanque={
-            125,
-            2500,
-            313,
-            313
-        };
-        int pedido = 500;
-
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        int [] result =
-            {
-                113,
-                2488,
-                300,
-                313
-
-
-            };
-        // testa posto comum
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.COMUM));
-
-        // testa posto estrategico
-        int [] result2 =
-            {
-                100,
-                2475,
-                288,
-                313
-            };
-        Assertions.assertEquals(result2, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.ESTRATEGICO));
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-    }
-
-    // ----------------------  <25%  ----------------------
-
-    @Test
-    public void Test5() {
-        int[] quantTanque={
-            124,
-            2500,
-            313,
-            313
-        };
-        int pedido = 500;
-
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        // testa posto comum
-        int [] resultComum = {-2,0,0,0};
-        Assertions.assertEquals(resultComum, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.COMUM));
+            tanques = new DepComb(quantTAditivo, quantTGasolina, quantTAlcool1, quantTAlcool2);
+            int[] resultEstrategico = {quantTAditivoResultEstrategico, quantTGasolinaResultEstrategico, quantTAlcool1ResultEstrategico, quantTAlcool2ResultEstrategico};
+            Assertions.assertEquals(resultEstrategico, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.ESTRATEGICO));
         
-        // testa posto estrategico
-        int [] result =
-            {
-                99,
-                2475,
-                288,
-                313
-            };
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.ESTRATEGICO));
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
     }
 
-
-    @Test
-    public void decimoPrimeiroTest() {
-        int[] quantTanque={
-            20,
-            90,
-            100,
-            30
-        };
-        int pedido = -50;
-
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-        int [] result = {-1,0,0,0};
-
-        // testa posto comum
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.COMUM));
-        
-        // testa posto estrategico
-        Assertions.assertEquals(result, tanques.encomendaCombustivel(pedido, DepComb.TIPOPOSTO.ESTRATEGICO));
-        tanques = new DepComb(quantTanque[0], quantTanque[1], quantTanque[2], quantTanque[3]);
-    }
 }
