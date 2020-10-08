2eb7eff23042e47a1db8b5f266f6678d2cc82eb5
Diego Klein <dgzklein1@hotmail.com>
2020-10-08 02:08:46 -0300

    Ajustes

--- my-app/src/main/java/com/mycompany/app/DepComb.java
+++ my-app/src/main/java/com/mycompany/app/DepComb.java
@@ -1,5 +1,8 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DepComb {
     public enum SITUACAO { NORMAL, SOBRAVISO, EMERGENCIA }
     public enum TIPOPOSTO { COMUM, ESTRATEGICO }
@@ -8,17 +11,30 @@ public class DepComb {
     public static final int MAX_ALCOOL = 2500;
     public static final int MAX_GASOLINA = 10000;
 
+    private int tAditivo;
+	private int tGasolina;
+	private int tAlcool1;
+	private int tAlcool2;
+
+    private SITUACAO sit;
+
     public DepComb(int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) { 
-        tGasolina = recebeGasolina(tGasolina);
-        tAditivo = recebeAditivo(tAditivo);
-        tAlcool1 = recebeAlcool(tAlcool1);
-        tAlcool2 = recebeAlcool(tAlcool2);
+        this.tAditivo = tAditivo;
+        this.tGasolina = tGasolina;
+        this.tAlcool1 = tAlcool1;
+        this.tAlcool2 = tAlcool2;     
+        defineSituacao(); 
      }
 
-    public void defineSituacao(){  }
+    public void defineSituacao(){ 
+        List<Double> comb = new ArrayList<>();
+        comb.add((double) tAditivo/MAX_ADITIVO);
+        comb.add((double) tGasolina/MAX_GASOLINA);
+        comb.add((double) (tAlcool1+tAlcool2)/MAX_ALCOOL);
+     }
 
     public SITUACAO getSituacao(){
-        return null;
+        return sit;
     }
 
     public int gettGasolina(){
@@ -42,14 +58,23 @@ public class DepComb {
      * @return -1 caso valor invalido
      */
     public int recebeAditivo(int qtdade) {
+        if(qtdade<1){
+            return -1;
+        }
         return qtdade;
     }
 
     public int recebeGasolina(int qtdade) {
+        if(qtdade<1){
+            return -1;
+        }
         return qtdade;
     }
 
     public int recebeAlcool(int qtdade) {
+        if(qtdade<1){
+            return -1;
+        }
         return qtdade;
     }
     /**
@@ -70,6 +95,12 @@ public class DepComb {
      *  - sem combustivel suficiente -> @return [-3,0,0,0]  postos estrategicos podem entregar tudo oq podem
      */
     public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
+        
+        
+        DepComb x = new DepComb(aditivo, gasolina, alcool1, alcool2);
+        
+
+
         return null;
     }
 
